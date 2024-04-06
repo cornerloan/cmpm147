d5e35853788f82c59c2bc608267e784ef9fac792
cornerloan <74088480+cornerloan@users.noreply.github.com>
2024-04-06 16:34:44 -0700

    Update project.js

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -35,11 +35,11 @@ function main() {
   missions: ["defend a friendly outpost", "protect liberty", "defend freedom", "search and destroy enemy outposts", "launch an ICBM", "conduct geological surveys", "upload escape pod data", "retrieve valuable data", "activate E-710 pumps", "evacuate civilians"]
 };
 
-const template = "Greetings $rank! We need your assistance on this mission.<BR>
-$planets is currently being attacked by a small army of $enemies.</br>
-Fortunately, we have some equipment for you to take. Your equipment will consist of:<br>
-1x $guns<\br>
-2x $grenades grenades<\n>
+const template = "Greetings $rank! We need your assistance on this mission.
+$planets is currently being attacked by a small army of $enemies.
+Fortunately, we have some equipment for you to take. Your equipment will consist of:
+1x $guns
+2x $grenades grenades
 2x $powers1
 3x Orbital $powers2
 1x Eagle $powers3
